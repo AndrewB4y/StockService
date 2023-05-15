93bfaeb615bc9feb6ddae6c00970c60c7368e220
AndrewB4y <andresbaymon@gmail.com>
2023-05-15 15:09:35 -0500

    [update] redirecting root endpoint to signup endpoint

--- stock_service/urls.py
+++ stock_service/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='auth/signup')),
     path('auth/', include('authentication.urls')),
     # jwt auth
     path('login/', views.TokenObtainPairView.as_view(), name='login'),
